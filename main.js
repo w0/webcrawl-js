b5f3312e1f799319d56cecb6b4dae6fadba5b935
w0 <git@kassidy.dev>
2024-08-13 22:16:28 -0500

    added main func

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-console.log("hello node world");
+import {argv} from 'node:process'
 
-console.log("howdy Hey");
+function main() {
+    if (process.argv.length != 3) {
+        throw new Error("You must specify one argument")
+    }
+    const baseURL = process.argv[2]
+    console.log(`Crawler Start: ${baseURL}`)
+}
+
+main()
