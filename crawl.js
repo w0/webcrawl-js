1b6f9df5688fd38c23cbbb6c5110501c6dbae9e9
w0 <git@kassidy.dev>
2024-08-18 11:47:48 -0500

    now crawls pages

--- crawl.js
+++ crawl.js
@@ -1,15 +1,15 @@
 function normalizeURL(url) {
   let someURL
   try {
-    someURL = new URL(url)   
+    someURL = new URL(url)
   } catch (err) {
     someURL = new URL(`http://${url}`)
   }
 
   let normalized = someURL.hostname
-  
+
   if (someURL.pathname.endsWith('/')) {
-    normalized = normalized.concat(someURL.pathname.slice(0,-1))
+    normalized = normalized.concat(someURL.pathname.slice(0, -1))
   } else {
     normalized = normalized.concat(someURL.pathname)
   }
@@ -41,4 +41,56 @@ function getURLsFromHTML(html, baseURL) {
   return foundURLs
 }
 
-export { normalizeURL, getURLsFromHTML }
+async function fetchURL(url) {
+  let currentPage
+  try {
+    currentPage = await fetch(url)
+  } catch (err) {
+    throw new Error(`Unable to fetch: ${err}`)
+  }
+
+  if (currentPage.status >= 400) {
+    console.log(new Error(`HTTP Error: ${currentPage.status}`))
+    return
+  }
+
+  const type = currentPage.headers.get('Content-Type')
+
+  if (!type.includes('text/html')) {
+    console.log(new Error(`Content-Type not text/html: ${type}, ${url}`))
+    return
+  }
+
+  return await currentPage.text()
+}
+
+async function crawlPage(baseURL, currentURL = baseURL, pages = {}) {
+  const currentURLObj = new URL(currentURL)
+  const baseURLObj = new URL(baseURL)
+
+  if (currentURLObj.hostname !== baseURLObj.hostname) {
+    return pages
+  }
+
+  const currentNormalized = normalizeURL(currentURL)
+
+  if (Object.hasOwn(pages, currentNormalized)) {
+    pages[currentNormalized]++
+    return pages
+  }
+
+  pages[currentNormalized] = 1
+
+  const body = await fetchURL("https://" + currentNormalized)
+
+  const foundURLs = getURLsFromHTML(body, baseURL)
+  
+  for (const url of foundURLs) {
+    pages = await crawlPage(baseURL, url, pages )
+  }
+
+  return pages
+
+}
+
+export { normalizeURL, getURLsFromHTML, crawlPage }
--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 import {argv} from 'node:process'
+import { crawlPage } from './crawl.js'
 
-function main() {
+async function main() {
     if (process.argv.length != 3) {
         throw new Error("You must specify one argument")
     }
     const baseURL = process.argv[2]
     console.log(`Crawler Start: ${baseURL}`)
+    const crawled = await crawlPage(baseURL)
+
+    console.log(crawled)
 }
 
 main()
